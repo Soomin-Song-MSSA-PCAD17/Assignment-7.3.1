a1323045fff21d83f607488e64f185a1f400774b
Soomin Song <d.song.149@gmail.com>
2025-05-21 09:28:32 -0700

    Link to leetcode

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿/// You are given the root of a binary search tree (BST) and an integer val.
+﻿/// Leetcode 700
+/// https://leetcode.com/problems/search-in-a-binary-search-tree
+/// You are given the root of a binary search tree (BST) and an integer val.
 /// Find the node in the BST that the node's value equals val and return the
 /// subtree rooted with that node. If such a node does not exist, return null.
 /**
